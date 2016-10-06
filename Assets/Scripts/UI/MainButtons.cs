f930c3dfadeea3f723049bb295eb1bcc97afd74d
Ivan <ivan5247@gmail.com>
2016-10-06 02:46:38 -0700

    Simple Main Menu
    ^

---
+++ Assets/Scripts/UI/MainButtons.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+
+public class MainButtons : MonoBehaviour {
+
+	public void StartGame(string newGame){
+		SceneManager.LoadScene (newGame);
+	}
+}
