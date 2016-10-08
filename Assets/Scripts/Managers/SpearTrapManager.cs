6cba102f891880b600cbbe8a70cd16c46268b905
Luke Westlake <c0mmissargaunt18@gmail.com>
2016-10-08 13:43:43 -0700

    Added Spear Trap Prefabs and Updated Level 1

---
+++ Assets/Scripts/Managers/SpearTrapManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpearTrapManager : MonoBehaviour {
+
+	Animator anim;
+	[SerializeField] private float timeBetweenAttacks = 5.0f;
+	[SerializeField] private float timeBeforeActivation = 0.0f;
+	private float attackCountdown;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+		attackCountdown = timeBetweenAttacks + timeBeforeActivation;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		attackCountdown = attackCountdown - Time.deltaTime;
+
+		if (attackCountdown < 0) 
+		{
+			anim.SetInteger ("State", 1); // Enter the attack animation
+			attackCountdown = timeBetweenAttacks; // Reset the countdown
+		} 
+		else if (anim.GetInteger("State") == 1) 
+		{
+			anim.SetInteger ("State", 0); //Go back to idle animation
+		}
+
+
+	}
+}
