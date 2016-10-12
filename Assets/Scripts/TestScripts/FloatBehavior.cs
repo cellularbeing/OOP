cf0468c7cbc0c7e5cb6d3fe1f4934fcf9fd2a1b7
Luke Westlake <c0mmissargaunt18@gmail.com>
2016-10-11 19:27:44 -0700

    Added Scripts for Floating Behavior and Hue Shift

---
+++ Assets/Scripts/TestScripts/FloatBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class FloatBehavior : MonoBehaviour
+{
+	float originalY;
+
+	public float floatStrength = 1; // You can change this in the Unity Editor to 
+	// change the range of y positions that are possible.
+	public float floatSpeed = 1;
+
+	void Start()
+	{
+		this.originalY = this.transform.position.y;
+	}
+
+	void Update()
+	{
+		transform.position = new Vector3(transform.position.x,
+			originalY + ((float)Math.Sin(Time.time * floatSpeed) * floatStrength),
+			transform.position.z);
+	}
+}
---
+++ Assets/Scripts/TestScripts/HueShift.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HueShift : MonoBehaviour {
+
+	public float Speed = 0.25f;
+	private SpriteRenderer rend;
+
+	void Start()
+	{
+		//gameObject.GetComponent<SpriteRenderer>().color=new Color(health/100f, health/100f, 1, 1);
+
+		rend = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rend.color = Color.HSVToRGB(Mathf.PingPong(Time.time * Speed, 1), 1, 1);
+	}
+}
