dd6b17c5f80100980fcaab461d42469a8c215427
Jeremyg71089 <Jeremygalarza71089@gmail.com>
2016-10-19 12:07:05 -0700

    Added an Enemy
    Added a Snake Enemy(Sprite, Scripts,Prefab) There are two scripts one
    for any enemy (Health and take Damage functions) and one for the
    movement of the snake. It moves left and right turning when it runs into
    an obstruction.

---
+++ Assets/Scripts/Behaviors/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	private int health;
+	private bool dead;
+	// Use this for initialization
+	void Start () {
+		dead = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0) {
+			dead = true;
+		}
+	}
+		
+	public bool isDead(){
+		return dead;
+	}
+	public void setHealth(int h){
+		health = h;
+		print (health);
+	}
+	public void takeDamage(){
+		health--;
+		print (health);
+	}
+
+	public void takeDamage(int d){
+		health = health - d;
+	}
+
+
+}
---
+++ Assets/Scripts/Behaviors/snakemovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class snakemovement : MonoBehaviour{
+	public float speed;
+	private Transform t;
+	private float prev;
+	private float newX;
+	private GameObject g;
+	private Rigidbody2D rb;
+	private BoxCollider2D bc;
+	private Enemy e;
+	public int health;
+	private Vector3 speedvec;
+	private SpriteRenderer sr;
+	// Use this for initialization
+	void Start () {
+		sr = gameObject.GetComponent<SpriteRenderer> ();
+		t = gameObject.GetComponent<Transform> ();
+		bc = gameObject.GetComponent<BoxCollider2D> ();
+		newX = t.position.x;
+		e = gameObject.GetComponent<Enemy>();
+		print (health);
+		e.setHealth (health);
+		speedvec = new Vector3 (speed,0,0);
+		prev = t.position.x;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		t.Translate (speedvec);
+		if (Mathf.Abs(prev - t.position.x) < speed/2) {
+			if (sr.flipX == true)
+				sr.flipX = false;
+			else
+				sr.flipX = true;
+			speedvec = speedvec * -1;
+		}
+		prev = t.position.x;
+	}
+}
